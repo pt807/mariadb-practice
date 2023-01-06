182ca2a435f1457bd5d9d025200ea3948c7b4166
pt807 <pt807@naver.com>
2023-01-06 15:10:55 +0900

    first commit

---
+++ sql연습/ex01.sql
@@ -0,0 +1,32 @@
+show databases;
+use webdb;
+
+
+-- 대소문자 구분 안한다.
+sElect version(), current_date(), nOw() from dual;
+
+-- 수학함수, 사칙 연산도 된다.
+select 1+2+3+4 sum from dual;
+SELECT SIN(PI() / 4) FROM DUAL;
+
+-- table 생성: DML
+CREATE TABLE pet (
+    name VARCHAR(100),
+    owner VARCHAR(20),
+    species VARCHAR(20),
+    gender CHAR(1),
+    birth DATE,
+    death DATE
+);
+
+-- schema 확인 describe
+desc pet;
+
+-- table 삭제
+drop table pet;
+show tables;
+
+-- insert: DML(C)
+insert into pet values(
+'ㄹㄹ', 'me', 'dog', 'm', '2000-12-12', null
+);
