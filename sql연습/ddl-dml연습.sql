37e8ed15b49bca710222303c58809eec9cc5d493
pt807 <pt807@naver.com>
2023-01-10 14:56:06 +0900

    dml & tx

--- sql연습/ddl-dml연습.sql
+++ sql연습/ddl-dml연습.sql
@@ -29,3 +29,28 @@ desc member;
 
 alter table member drop juminbunho;
 desc member;
+
+-- insert
+insert into member values(null, 'asas@cas.com', password('1234'), '박대준', '개발팀', null);
+select * from member;
+
+insert into member(no, email, name, dept, password) values(null, 'asas2@cas.com', '박대준', '개발팀', password('1234'));
+
+-- update
+update member set email = 'asas3@cas.com', password = password('5678') where no = 2;
+
+-- delete
+delete from member where no = 2;
+
+
+--  create user 'webdb'@'localhost' identified by 'webdb';
+--  grant all privileges on webdb.* to 'webdb'@'localhost';
+
+-- transaction
+select @@autocommit;
+set autocommit=0;
+
+insert into member(no, email, name, dept, password) values(null, 'asas5@cas.com', '박대준', '개발팀', password('1234'));
+select * from member;
+
+commit;
